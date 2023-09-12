1d8db6b7e719d2f42f437c948dbda7cd064b0a8f
Fernando Carrillo Garcia <ferduzi007fcg@hotmail.com>
2023-09-12 00:08:18 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once("recursos/config/db.php");
+require_once("vistas/footer/navbar.php");
+
+if((!empty($_GET["page"])) && (isset($_GET["page"]))){
+    $page = $_GET["page"];
+}
+switch($page){
+    case "servicios":
+        include_once("vistas/users/servicios.php");
+        break;
+    default:
+        include_once("vistas/home/Home.php");
+        break;
+}
+
+require_once("vistas/footer/footer.php");
+
+?>
