800b17f6e67c216b4fc203b2df4e530dcc702d61
Ismael <ismael.guillermo@hotmail.com>
2023-09-16 13:19:00 -0600

    nav-bar inicial y css

---
+++ vistas/navbar/navbar.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link rel="stylesheet" type="text/css" href="/recursos/CSS/nav-bar.css">
+</head>
+
+<body>
+    <nav class="custom-navbar">
+        <ul class="custom-navbar-list">
+            <li class="custom-navbar-item"><a class="custom-navbar-link" href="#inicio">Inicio</a></li>
+            <li class="custom-navbar-item"><a class="custom-navbar-link" href="#servicios">Servicios</a></li>
+            <li class="custom-navbar-item"><a class="custom-navbar-link" href="#nosotros">Nosotros</a></li>
+            <li class="custom-navbar-item"><a class="custom-navbar-link" href="#clientes">Nuestros Clientes</a></li>
+            <li class="custom-navbar-item"><a class="custom-navbar-link" href="#contacto">Contacto</a></li>
+        </ul>
+    </nav>
+</body>
+
+</html>
