0db329adcb0ce2f323c76222f30dc35ee56815d9
Ismael <ismael.guillermo@hotmail.com>
2023-09-24 20:52:01 -0600

    mejora pagina servicios

--- vistas/users/clientes/Servicios.php
+++ vistas/users/clientes/Servicios.php
@@ -1,31 +1,11 @@
-<?php
-require_once("modelos/model_archivos.php");
-require_once("modelos/model_publicaciones.php");
-
-if (isset($_GET['ins'])) {
-    if ($_GET['ins'] == "Ok") {
-        echo '<script>alert("Se inserto correctamente");</script>';
-    }
-} elseif (isset($_GET['upd'])) {
-    if ($_GET['upd'] == "Ok") {
-        echo '<script>alert("Se actualizo correctamente");</script>';
-    }
-} elseif (isset($_GET['del'])) {
-    if ($_GET['del'] == "Ok") {
-        echo '<script>alert("Se elimino correctamente");</script>';
-    }
-}
-?>
-
 <!DOCTYPE html>
 <html lang="es">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Galería de Servicios</title>
+    <title>Galería de Productos</title>
     <style>
-        /* Estilos generales */
         body {
             font-family: Arial, sans-serif;
             background-color: #f2f2f2;
@@ -35,8 +15,11 @@ if (isset($_GET['ins'])) {
 
         h1 {
             text-align: center;
-            padding: 20px 0;
-            color: #333;
+            background: linear-gradient(45deg, #ff6600, #007BFF);
+            -webkit-background-clip: text;
+            background-clip: text;
+            color: transparent;
+            padding-bottom: 10px;
         }
 
         .gallery {
@@ -62,71 +45,164 @@ if (isset($_GET['ins'])) {
             margin: 0;
         }
 
-        .images {
-            display: flex;
-            flex-wrap: nowrap;
-            overflow-x: auto;
-            padding: 20px;
+        .carousel-container {
+            overflow: hidden;
+            position: relative;
         }
 
-        .images img {
-            width: 0;
-            flex-grow: 1;
+        .image-carousel {
+            display: flex;
+            transition: transform 0.5s ease;
+        }
+
+        .image {
+            flex: 0 0 calc(100% / 5);
+            position: relative;
+            margin-right: 10px;
+            overflow: hidden;
+            cursor: pointer;
+        }
+
+        .image img {
+            width: 100%;
+            height: auto;
             object-fit: cover;
             opacity: 0.8;
-            transition: width 0.2s ease, opacity 0.2s ease, filter 0.2s ease;
-            margin-right: 10px;
+            transition: transform 0.2s ease, opacity 0.2s ease, filter 0.2s ease;
         }
 
-        .images img:last-child {
-            margin-right: 0;
-        }
-
-        .images img:hover {
-            cursor: crosshair;
-            width: 300px;
+        .image:hover img {
+            transform: scale(1.1);
             opacity: 1;
             filter: contrast(120%);
         }
 
-        /* Estilos para dispositivos móviles */
+        .description {
+            position: absolute;
+            background-color: rgba(0, 0, 0, 0.7);
+            color: #fff;
+            padding: 10px;
+            opacity: 0;
+            transition: opacity 0.2s ease;
+            top: 0;
+            left: 0;
+            right: 0;
+            bottom: 0;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            text-align: center;
+        }
+
+        .image:hover .description {
+            opacity: 1;
+        }
+
         @media (max-width: 768px) {
             .category {
                 width: 100%;
             }
         }
+
+        .navigation-arrows {
+            position: absolute;
+            width: 30px;
+            height: 30px;
+            background-color: rgba(0, 0, 0, 0.5);
+            color: #fff;
+            text-align: center;
+            font-size: 24px;
+            cursor: pointer;
+            z-index: 2;
+            border-radius: 50%;
+            transition: background-color 0.3s ease;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+        }
+
+        .navigation-arrows.left {
+            left: 10px;
+            top: auto;
+            bottom: 10px;
+        }
+
+        .navigation-arrows.right {
+            right: 10px;
+            top: auto;
+            bottom: 10px;
+        }
+
+        .navigation-arrows:hover {
+            background-color: rgba(0, 0, 0, 0.8);
+        }
     </style>
 </head>
 
 <body>
+    <h1>Nuestros Servicios</h1>
     <section class="gallery">
         <div class="category">
             <h2>Lavado</h2>
-            <div class="images">
-                <img src="descarga2.png" alt="Plomería 1">
-                <img src="descarga2.png" alt="Plomería 2">
-                <img src="descarga2.png" alt="Plomería 3">
-                <img src="descarga2.png" alt="Plomería 4">
-                <img src="descarga2.png" alt="Plomería 5">
-                <img src="descarga2.png" alt="Plomería 6">
-                <img src="descarga2.png" alt="Plomería 1">
-                <img src="descarga2.png" alt="Plomería 2">
-                <img src="descarga2.png" alt="Plomería 3">
-                <img src="descarga2.png" alt="Plomería 4">
-                <img src="descarga2.png" alt="Plomería 5">
-                <img src="descarga2.png" alt="Plomería 6">
-                <img src="descarga2.png" alt="Plomería 1">
-                <img src="descarga2.png" alt="Plomería 2">
-                <img src="descarga2.png" alt="Plomería 3">
-                <img src="descarga2.png" alt="Plomería 4">
-                <img src="descarga2.png" alt="Plomería 5">
-                <img src="descarga2.png" alt="Plomería 6">
-                <img src="descarga2.png" alt="Plomería 1">
-                <img src="descarga2.png" alt="Plomería 2">
-                <img src="descarga2.png" alt="Plomería 3">
-                <img src="descarga2.png" alt="Plomería 4">
-                <img src="descarga2.png" alt="Plomería 5">
-                <img src="descarga2.png" alt="Plomería 6">
+            <!-- Contenedor del carrusel -->
+            <div class="carousel-container">
+                <!-- Flechas de navegación -->
+                <div class="navigation-arrows left" id="prevButton">&#9664;</div>
+                <div class="image-carousel">
+                    <!-- Imagen 1 con descripción -->
+                    <div class="image">
+                        <img src="descarga2.png" alt="Producto 1">
+                        <div class="description">Descripción del Producto 1.</div>
+                    </div>
+                    <!-- Repite este patrón para más imágenes -->
+                    <div class="image">
+                        <img src="descarga2.png" alt="Producto 2">
+                        <div class="description">Descripción del Producto 2.</div>
+                    </div>
+                    <div class="image">
+                        <img src="descarga2.png" alt="Producto 3">
+                        <div class="description">Descripción del Producto 3.</div>
+                    </div>
+                    <div class="image">
+                        <img src="descarga2.png" alt="Producto 4">
+                        <div class="description">Descripción del Producto 4.</div>
+                    </div>
+                    <div class="image">
+                        <img src="descarga2.png" alt="Producto 5">
+                        <div class="description">Descripción del Producto 5.</div>
+                    </div>
+                    <!-- Repite este patrón para más imágenes -->
+                    <div class="image">
+                        <img src="descarga2.png" alt="Producto 6">
+                        <div class="description">Descripción del Producto 6.</div>
+                    </div>
+                    <!-- Puedes agregar más imágenes aquí -->
+                    <div class="image">
+                        <img src="descarga2.png" alt="Producto 7">
+                        <div class="description">Descripción del Producto 7.</div>
+                    </div>
+                    <div class="image">
+                        <img src="descarga2.png" alt="Producto 8">
+                        <div class="description">Descripción del Producto 8.</div>
+                    </div>
+                    <div class="image">
+                        <img src="descarga2.png" alt="Producto 9">
+                        <div class="description">Descripción del Producto 9.</div>
+                    </div>
+                    <div class="image">
+                        <img src="descarga2.png" alt="Producto 10">
+                        <div class="description">Descripción del Producto 10.</div>
+                    </div>
+                    <div class="image">
+                        <img src="descarga2.png" alt="Producto 11">
+                        <div class="description">Descripción del Producto 11.</div>
+                    </div>
+                    <div class="image">
+                        <img src="descarga2.png" alt="Producto 11">
+                        <div class="description">Descripción del Producto 11.</div>
+                    </div>
+                </div>
+                <div class="navigation-arrows right" id="nextButton">&#9654;</div>
             </div>
         </div>
     </section>
